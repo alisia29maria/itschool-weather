3fcdd349a1c6b58c0eea1731a0462d681904dc9c
alisia29maria <alisia29maria@gmail.com>
2023-03-06 16:16:29 +0200

    initialize project

---
+++ README.md
@@ -0,0 +1,7 @@
+# itschool-weather
+
+The JS project from my web development course @ IT School
+
+## Demo
+
+The project is deployed here: https://itschool-weather-second-project.netlify.app/
---
+++ api/endpoints.js
@@ -0,0 +1,9 @@
+const API_KEY = "dafd4a88c14bea99f85c63957004c87a";
+
+function getCurrentWeatherEndpoint(city) {
+  return `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ro&units=metric&appid=${API_KEY}`;
+}
+
+function getForecastEndpoint(city) {
+  return `https://api.openweathermap.org/data/2.5/forecast?q=${city}&lang=ro&units=metric&appid=${API_KEY}`;
+}
